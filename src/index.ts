871b0313a185f440cedfa4eb4a9e2d29c4cdb005
Hogan, Shahn <shogan@midwayusa.com>
2022-10-10 07:13:41 -0500

    Add isJust, isNothing and constructors

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,83 @@
-import { curry } from 'ramda'
+import { curry } from "ramda";
 
 export enum MaybeType {
-  Just = 'just',
-  Nothing = 'nothing',
+  Just = "just",
+  Nothing = "nothing",
 }
 
 export interface Just<T> {
-  type: typeof MaybeType.Just
-  value: T
+  type: typeof MaybeType.Just;
+  value: T;
 }
 
 export interface Nothing {
-  type: typeof MaybeType.Nothing
+  type: typeof MaybeType.Nothing;
 }
 
-export type Maybe<T>
-  = Just<T>
-  | Nothing
+export type Maybe<T> = Just<T> | Nothing;
 
-function maybeOf<T> (value: T): Maybe<T> {
+export const Nothing = (): Nothing => ({
+  type: MaybeType.Nothing,
+});
+
+export const Just = <T>(value: T): Just<T> => ({
+  type: MaybeType.Just,
+  value,
+});
+
+function maybeOf<T>(value: T): Maybe<T> {
   return value === undefined || value === null
     ? { type: MaybeType.Nothing }
-    : { type: MaybeType.Just, value }
+    : { type: MaybeType.Just, value };
 }
 
-function maybeMap<A, B> (f: (val: A) => B, m: Maybe<A>): Maybe<B> {
+function maybeMap<A, B>(f: (val: A) => B, m: Maybe<A>): Maybe<B> {
   switch (m.type) {
     case MaybeType.Nothing:
-      return { ...m }
+      return { ...m };
     case MaybeType.Just:
-      return { ...m, value: f(m.value) }
+      return { ...m, value: f(m.value) };
   }
 }
 
-function maybeAndThen<A, B> (f: (val: A) => Maybe<B>, m: Maybe<A>): Maybe<B> {
+function maybeAndThen<A, B>(f: (val: A) => Maybe<B>, m: Maybe<A>): Maybe<B> {
   switch (m.type) {
     case MaybeType.Nothing:
-      return { ...m }
+      return { ...m };
     case MaybeType.Just:
-      return f(m.value)
+      return f(m.value);
   }
 }
 
-function maybeWithDefault<T> (defaultVal: T, m: Maybe<T>): T {
+function maybeWithDefault<T>(defaultVal: T, m: Maybe<T>): T {
   switch (m.type) {
     case MaybeType.Nothing:
-      return defaultVal
+      return defaultVal;
     case MaybeType.Just:
-      return m.value
+      return m.value;
   }
 }
 
-function maybeAp<A, B> (f: Maybe<(val: A) => B>, m: Maybe<A>): Maybe<B> {
+function maybeAp<A, B>(f: Maybe<(val: A) => B>, m: Maybe<A>): Maybe<B> {
   return f.type == MaybeType.Nothing || m.type == MaybeType.Nothing
     ? { type: MaybeType.Nothing }
-    : { type: MaybeType.Just, value: f.value(m.value) } 
+    : { type: MaybeType.Just, value: f.value(m.value) };
+}
+
+function maybeIsJust<T>(m: Maybe<T>): boolean {
+  return m.type == MaybeType.Just;
+}
+
+function maybeIsNothing<T>(m: Maybe<T>): boolean {
+  return m.type == MaybeType.Nothing;
 }
 
 export const Maybe = {
   andThen: curry(maybeAndThen),
   ap: curry(maybeAp),
+  isJust: maybeIsJust,
+  isNothing: maybeIsNothing,
   map: curry(maybeMap),
   of: maybeOf,
   withDefault: curry(maybeWithDefault),
-}
+};
